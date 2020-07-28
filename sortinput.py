0dd10463d8d306279dfe51bb6cbdc610d19fa3c9
MathTauAthogen <34319964+MathTauAthogen@users.noreply.github.com>
2020-07-28 00:07:03 -0400

    Who needs testing? (But seriously someone test this)

--- sortinput.py
+++ sortinput.py
@@ -1,13 +1,15 @@
- //TODO: Kill stage directions (using regex to delete all between brackets)
- c = []
- t = raw_input()
- while(t != "000"):
-     c += [t] 
-     t = raw_input()
- te = []
- for i in c:
+//TODO: Kill stage directions (using regex to delete all between brackets)
+with open("play.txt","r") as f:
+     c = []
+     t = f.readline()
+     while(t != "000"):
+         c += [t]
+         t = f.readline()
+te = []
+for i in c:
      b = ".".join(i.split(".")[1:]).lower().split(" ")
      b.sort()
      b=" ".join([i.split(".")[0]+"."]+b)
-     te += [b] 
- print("\n".join(te))
+     te += [b]
+with open("newplay.txt","w") as f:
+     f.write("\n".join(te))
